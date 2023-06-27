6b5c58d25d5aa11ad3f8a07497c86d57254b7fca
panitarn <panitarn.work@gmail.com>
2023-06-27 22:03:26 +0700

    Initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_pdf.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
